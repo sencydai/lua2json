e921f3d2913eff1c2e0cc8f0564c6750648c6c51
sencydai <312905904@qq.com>
2018-04-17 15:25:42 +0800

    更新

--- main.go
+++ main.go
@@ -6,32 +6,74 @@ import (
 	"fmt"
 	luajson "github.com/layeh/gopher-json"
 	"github.com/yuin/gopher-lua"
+	"io/ioutil"
 	"os"
+	"strings"
 )
 
+type gConfig struct {
+	LuaFile  string
+	JsonFile string
+}
+
 func main() {
+	defer func() {
+		var s string
+		fmt.Scanln(&s)
+	}()
+
+	config := gConfig{}
+	if data, err := ioutil.ReadFile("config.json"); err != nil {
+		fmt.Println(err.Error())
+		return
+	} else if err = json.Unmarshal(data, &config); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
 	L := lua.NewState()
 	defer L.Close()
 	luajson.Preload(L)
 
-	err := L.DoFile("TestConfig.lua")
-	if err != nil {
-		fmt.Println("DoFile:", err.Error())
+	if err := os.MkdirAll(config.JsonFile, os.ModeDir); err != nil {
+		fmt.Println(err.Error())
 		return
 	}
-	v := L.GetGlobal("TestConfig")
-	buff, err := luajson.Encode(v)
-	if err != nil {
-		fmt.Println("Encode:", err.Error())
+
+	if files, err := ioutil.ReadDir(config.LuaFile); err != nil {
+		fmt.Println(err.Error())
 		return
+	} else {
+		for _, file := range files {
+			if file.IsDir() {
+				continue
+			}
+			name := file.Name()
+			if !strings.HasSuffix(name, ".lua") {
+				continue
+			}
+			err := L.DoFile(fmt.Sprintf("%s/%s", config.LuaFile, name))
+			if err != nil {
+				fmt.Printf("load file %s error: %s", name, err.Error())
+				continue
+			}
+			v := L.GetGlobal(strings.TrimSuffix(name, ".lua"))
+			buff, err := luajson.Encode(v)
+			if err != nil {
+				fmt.Printf("parse file %s error: %s", name, err.Error())
+				continue
+			}
+			file, err := os.Create(fmt.Sprintf("%s/%s.json", config.JsonFile, strings.TrimSuffix(name, ".lua")))
+			if err != nil {
+				fmt.Printf("create json file %s error: %s", name, err.Error())
+				continue
+			}
+			writer := bytes.NewBuffer([]byte{})
+			json.Indent(writer, buff, "", "\t")
+			file.Write(writer.Bytes())
+			file.Close()
+		}
 	}
-	file, err := os.Create("TestConfig.json")
-	if err != nil {
-		fmt.Println("Create:", err.Error())
-		return
-	}
-	defer file.Close()
-	writer := bytes.NewBuffer([]byte{})
-	json.Indent(writer, buff, "", "\t")
-	file.Write(writer.Bytes())
+
+	fmt.Println("转换完成...")
 }
